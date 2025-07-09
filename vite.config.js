fce7ce7bfa00526794ffe491f40dc3eda3a5f6e7
sammy171207 <sameerrandive171207@gmail.com>
2025-07-09 22:40:50 +0530

    ..

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/Hammer_WebPage/',
+    base: '/',
   plugins: [react(),tailwindcss()],
 })
